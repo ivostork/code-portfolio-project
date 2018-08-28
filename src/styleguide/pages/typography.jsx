9f8a7267c8cbafc6cba4e8f68238a5a27fd2eb02
Ivo Stork <ivos.stork@gmail.com>
2018-08-28 06:35:02 +0200

    font files

--- src/styleguide/pages/typography.jsx
+++ src/styleguide/pages/typography.jsx
@@ -4,7 +4,14 @@ export default class Typography extends React.Component {
   render() {
     return (
       <div>
-        Typography
+        <h1>
+          Typography
+        </h1>
+        <br/>
+        <h2>
+          Headings
+        </h2>
+        
       </div>
     );
   }
