d80c0ef0d86109c37e0c4935393d3ce725dc0ec3
Ivo Stork <ivos.stork@gmail.com>
2018-08-28 05:29:44 +0200

    styleguide grid

--- src/styleguide/pages/grid.jsx
+++ src/styleguide/pages/grid.jsx
@@ -7,7 +7,95 @@ export default class Grid extends React.Component {
       <div>
         <h1>Grid</h1>
         <br/>
-        
+        <p>
+         The parent class is <strong>.row</strong> and the sibling class is <strong>.col-auto</strong>
+        </p>
+        <br/>
+        <div className="row">
+          <div className="col-auto">
+            <div className="grid-col">
+              .col-auto
+            </div>
+          </div>
+          <div className="col-auto">
+            <div className="grid-col">
+              .col-auto
+            </div>
+          </div>
+          <div className="col-auto">
+            <div className="grid-col">
+              .col-auto
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br/>
+        <br/>
+        <p>
+         The parent class is <strong>.row.narrow-gutter</strong> and the sibling class is <strong>.col-auto</strong>
+        </p>
+        <br/>
+        <div className="row narrow-gutter">
+          <div className="col-auto">
+            <div className="grid-col">
+              .col-auto
+            </div>
+          </div>
+          <div className="col-auto">
+            <div className="grid-col">
+              .col-auto
+            </div>
+          </div>
+          <div className="col-auto">
+            <div className="grid-col">
+              .col-auto
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br/>
+        <p>
+         The parent class is <strong>.row</strong> and the sibling class is <strong>.col</strong>
+        </p>
+        <br/>
+        <div className="row">
+          <div className="col">
+            <div className="grid-col">
+              .col
+            </div>
+          </div>
+          <div className="col">
+            <div className="grid-col">
+              .col
+            </div>
+          </div>
+          <div className="col">
+            <div className="grid-col">
+              .col
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br/>
+        <p>
+         The parent class is <strong>.row</strong> and the sibling classes are <strong>.col-md-4.col-xs-12</strong> and <strong>.col-md-8.col-xs-12</strong> 
+        </p>
+        <br/>
+        <div className="row">
+          <div className="col-md-4 col-xs-12">
+            <div className="grid-col">
+              .col-md-4.col-xs-12
+            </div>
+          </div>
+          <div className="col-md-8 col-xs-12">
+            <div className="grid-col">
+              .col-md-8.col-xs-12
+            </div>
+          </div>          
+        </div>
       </div>
     );
   }
