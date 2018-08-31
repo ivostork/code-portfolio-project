2389e8e730c7b88c773374977df0c9762d28a6cb
Ivo Stork <ivos.stork@gmail.com>
2018-08-31 21:20:05 +0200

    badges

--- src/apps/styleguide/pages/badges.jsx
+++ src/apps/styleguide/pages/badges.jsx
@@ -4,9 +4,26 @@ import React from "react";
 export default class Badges extends React.Component {
   render() {
     return (
-      <h1>
-        Badges
-      </h1>
+      <div>
+        <h1>
+          Badges
+        </h1>
+        <div className="mb-20">
+          <span className="badge-status-active">
+            active
+          </span>
+        </div>
+        <div className="mb-20">
+          <span className="badge-status-pending">
+            pending
+          </span>
+        </div>
+        <div className="mb-20">
+          <span className="badge-status-closed">
+            closed
+          </span>
+        </div>
+      </div>
     );
   }
 }
